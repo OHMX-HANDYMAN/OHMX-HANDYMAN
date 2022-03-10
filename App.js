eac960d425c9b6d0d21feaadd710a32656188afa
OHMX-HANDYMAN <101319724+OHMX-HANDYMAN@users.noreply.github.com>
2022-03-10 13:58:18 +0530

    Add my files

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.rootblock}>
+      <Text>ohmx!</Text>
+      <StatusBar style="auto" />
+      <Button title="Empty" onPress={buttonPressed} style = {styles.buttonStyle}/>
+    </View>
+  );
+
+  function buttonPressed(){
+    console.log("button pressed")
+  }
+}
+const styles = StyleSheet.create({
+  rootblock: {
+    flex: 1,
+    backgroundColor: '#008000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonStyle: {
+    flex: 1,
+    backgroundColor: '#008000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
